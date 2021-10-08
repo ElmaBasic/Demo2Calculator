84aaff18a242eda4701e051b278db66d7fb34b04
ElmaBasic <elma-basic@outlook.com>
2021-10-08 19:58:56 +0200

    variablen deklariert

--- src/Calculator.java
+++ src/Calculator.java
@@ -1,7 +1,12 @@
 public class Calculator {
 
     public static void main(String[] args){
-
+        int a = 1;
+        double d = 2.0;
+        boolean b = true;
+        char c = 'c';
+        String s = "Ich bin ein String";
+        System.out.println(s);
 
     }
 }
