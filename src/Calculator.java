c78465c4d682eb8d54790cb92703fdcebc415e85
ElmaBasic <elma-basic@outlook.com>
2021-10-08 20:02:04 +0200

    variablen neu deklariert

--- src/Calculator.java
+++ src/Calculator.java
@@ -1,10 +1,17 @@
+import java.sql.SQLOutput;
+
 public class Calculator {
 
     public static void main(String[] args){
         int a = 1;
+        System.out.print(a);
         double d = 2.0;
+        System.out.print(d);
         boolean b = true;
+        System.out.print(b);
+
         char c = 'c';
+        System.out.println(c);
         String s = "Ich bin ein String";
         System.out.println(s);
 
