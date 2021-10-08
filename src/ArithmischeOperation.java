e62b797087f099e40313ddffb1bce1647085f1db
ElmaBasic <elma-basic@outlook.com>
2021-10-08 20:23:06 +0200

    calculation

---
+++ src/ArithmischeOperation.java
@@ -0,0 +1,20 @@
+import java.sql.SQLOutput;
+
+public class ArithmischeOperation {
+
+    public static void main(String[]args){
+
+        int a = 6;
+        int b = 3;
+        System.out.println(a + b);
+        System.out.println(a - b);
+        System.out.println(a * b);
+        System.out.println(a / b);
+        System.out.println(a % b);
+
+        int multi = 3 * 3;
+        int p = 2 + multi;
+        System.out.println(p);
+
+    }
+}
